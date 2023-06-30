56336ad0ab78cdf466e5faaaa9e6d4e4258b93da
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-30 20:51:29 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,10 @@
+import { Hono } from "https://deno.land/x/hono@v3.2.7/mod.ts"
+import { serve } from "https://deno.land/std@0.192.0/http/server.ts"
+
+const app = new Hono()
+
+app.get('/realtime', async ctx => {
+  app.header("Access-Control-Allow-Origin", "*")
+  return await fetch("https://www-sk.icrr.u-tokyo.ac.jp/realtimemonitor/skev.gif")
+})
+serve(app.fetch)
