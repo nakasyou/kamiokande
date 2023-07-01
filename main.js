5c03235f7156e606d4b7a3715b70950369f2fa94
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-01 17:24:03 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap');
+
+body {
+  font-family: Sans-Serif;
+  margin: 0;
+}
+#saigen {
+  display: grid;
+  grid-template-columns: repeat(48,5px);
+}
+#saigen div {
+  height: 5px;
+}
+.meta {
+  color: #fff;
+}
+.meta a {
+  color: #fff;
+}
+.font-mono {
+  font-family: 'Roboto Mono', monospace;
+}
